05e0671f72e9978fac1db7de9c0a41e86e6b7771
tao95 <taoyuhong86@gmail.com>
2018-10-16 16:22:07 +0800

    Site updated: 2018-10-16 16:22:06

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/tao95/blog-storage/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/tao95/blog-storage/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/tao95/blog-storage/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/tao95/blog-storage/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
